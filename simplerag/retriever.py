3223a344ca489182a6abd01bf6b5a7b80c8386d2
Noah <noahc1510@gmail.com>
2025-03-13 19:44:55 +0800

    ! fix(retriever): Changed the default top_k from 6 to 10 to match the reranker

--- simplerag/retriever.py
+++ simplerag/retriever.py
@@ -5,7 +5,7 @@ class Retriever():
     def __init__(self, kb_svc:KBService) -> None:
         self.vectorstore = kb_svc.vectorstore
 
-    def query(self, input_query: str, top_k=6):
+    def query(self, input_query: str, top_k=10):
         # TODO: 在这里重新设计retriever和langchain解耦
         self.retriever:VectorStoreRetriever = self.vectorstore.as_retriever(
             search_type="similarity",
